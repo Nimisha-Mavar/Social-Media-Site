3e70280037563fd9a9d74be3d042be9efb3f93aa
Nimisha Mavar <112267753+Nimisha-Mavar@users.noreply.github.com>
2023-07-08 20:15:34 -0400

    Create unlike functionality by using deleteDoc function of Firestore

--- Social_Media/src/componets/Like.tsx
+++ Social_Media/src/componets/Like.tsx
@@ -1,5 +1,13 @@
 import { auth, db } from "../config/firebase";
-import { addDoc, collection, getDocs, query, where } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  getDocs,
+  query,
+  where,
+  deleteDoc,
+  doc,
+} from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 
@@ -9,6 +17,7 @@ interface postData {
 
 interface likeSchema {
   userId: string;
+  likeId: string;
 }
 export const Like = (postId: postData) => {
   const [user] = useAuthState(auth);
@@ -19,19 +28,52 @@ export const Like = (postId: postData) => {
 
   const getLikes = async () => {
     const data = await getDocs(likeData);
-    setLikes(data.docs.map((doc) => ({ userId: doc.data().uid })));
+    setLikes(
+      data.docs.map((doc) => ({ userId: doc.data().uid, likeId: doc.id }))
+    );
   };
 
   const addLike = async () => {
     try {
-      await addDoc(likeRef, { postid: postId.postId, uid: user?.uid });
+      const addNew = await addDoc(likeRef, {
+        postid: postId.postId,
+        uid: user?.uid,
+      });
       if (user) {
         setLikes((prev) =>
-          prev ? [...prev, { userId: user.uid }] : [{ userId: user.uid }]
+          prev
+            ? [...prev, { userId: user.uid, likeId: addNew.id }]
+            : [{ userId: user.uid, likeId: addNew.id }]
         );
       }
     } catch (err) {
       console.log(err);
+    }
+  };
+
+  const deleteLike = async () => {
+    {
+      /*for select the deleted document*/
+    }
+    const deleteData = query(
+      likeRef,
+      where("postid", "==", postId.postId),
+      where("uid", "==", user?.uid)
+    );
+
+    {
+      /*for the get an document from the select query*/
+    }
+    const getDeleteDoc = getDocs(deleteData);
+
+    {
+      /*for get and docid from the document  */
+    }
+    const getDeleteId = doc(db, "Likes", (await getDeleteDoc).docs[0].id);
+    const likeId = (await getDeleteDoc).docs[0].id;
+    await deleteDoc(getDeleteId);
+    if (user) {
+      setLikes((prev) => prev && prev.filter((like) => like.likeId !== likeId));
     }
   };
 
@@ -48,10 +90,14 @@ export const Like = (postId: postData) => {
           className={
             isUserLikes ? "bi bi-heart-fill float-end" : "bi bi-heart float-end"
           }
-          onClick={addLike}
+          onClick={isUserLikes ? deleteLike : addLike}
         />
         <br />
-        {likes && <p>{likes.length}</p>}
+        {likes && (
+          <p style={{ fontSize: "10px" }} className="float-end">
+            {likes.length}
+          </p>
+        )}
       </div>
     </>
   );
